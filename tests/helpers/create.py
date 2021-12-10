7af0251caa58e2b0678aa4fead7cef94732dd6aa
Priya <92863151+priya1puresoftware@users.noreply.github.com>
2021-12-10 15:44:00 +0530

    Update create.py

--- tests/helpers/create.py
+++ tests/helpers/create.py
@@ -11,5 +11,5 @@ def create_file(path: BasePath, contents: bytes = b"asdf") -> None:
         f.write(contents)
 
 
-def unsafe_create_file(path: BasePath, contents: bytes = b"asdf") -> None:
+def unsafe_create_file(path: BasePath, contents: bytes = b"asdf") -> asyncio.Task[None]:
     return asyncio.create_task(bbb.write.write_single(path, contents, overwrite=True))
